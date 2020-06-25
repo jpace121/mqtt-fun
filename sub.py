f5f3d25f4385d08e67ecc7d7cb33f5fe8ccd056a
James Pace <jpace121@gmail.com>
2020-06-25 01:37:42 +0000

    Teach sub to do rpc.

--- sub.py
+++ sub.py
@@ -7,16 +7,26 @@ class SubNode:
                                           transport="tcp")
         self._client.connect("localhost")
 
-        self._client.subscribe("logging")
+        self._client.subscribe([("logging", paho.mqtt.client.SubscribeOptions(qos=1)),
+                                ("hello", paho.mqtt.client.SubscribeOptions(qos=1))])
         self._client.message_callback_add("logging", self._callback)
+        self._client.message_callback_add("hello", self._rpc_callback)
         self.a = 0
 
     def loop(self):
-        self._client.loop_forever()
+        self._client.loop_start()
 
     @staticmethod
     def _callback(client, userdata, message):
-        print("Got one")
+        userdata.a = userdata.a + 1
+        print("{}".format(userdata.a))
+
+    @staticmethod
+    def _rpc_callback(client, userdata, message):
+        print("Message from: {}".format(message.payload))
+        userdata._client.publish(message.properties.ResponseTopic, "yo, {}".format(message.payload))
 
 a = SubNode()
 a.loop()
+while True:
+    pass;
